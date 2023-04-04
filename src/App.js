e0fc70f570faccc28eda4cb46f7de22be87b1d43
PIara-Adviters <capacitaciones4@adviters.com>
2023-04-04 16:36:08 -0300

    Corrige paths

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./Pages/LoginPage/LoginPage";
 import Layout from "./Pages/Layout/Layout";
 import Loading from "./components/Loading/Loading";
+import CargarLicenciaPage from "./Pages/CargarLicenciaPage/CargarLicenciaPage";
+// import Weather from "./components/Weather/Weather";
 
 function App() {
   return (
@@ -10,8 +12,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
-          <Route path="/loading" element={<Loading/>} />
+            <Route path="/loading" element={<Loading/>} />
             <Route index element={<LoginPage />} />
+            <Route path="/cargarLicencia" element={<CargarLicenciaPage/>}/>
             {/* <Route path="*" element={<ErrorPage />} /> */}
           </Route>
         </Routes>
