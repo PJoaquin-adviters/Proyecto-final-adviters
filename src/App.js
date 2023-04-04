01e9f85334ac500780fe458a1ca11be8ea064387
PIara-Adviters <capacitaciones4@adviters.com>
2023-04-04 11:34:48 -0300

    Corrige path Loading

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./Pages/LoginPage/LoginPage";
 import Layout from "./Pages/Layout/Layout";
-import Loading from "./components/Loading";
+import Loading from "./components/Loading/Loading";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/loading" element={<Loading/>} />
           <Route path="/" element={<Layout />}>
+          <Route path="/loading" element={<Loading/>} />
             <Route index element={<LoginPage />} />
             {/* <Route path="*" element={<ErrorPage />} /> */}
           </Route>
