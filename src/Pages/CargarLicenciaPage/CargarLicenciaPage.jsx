24817f361c08e663c861c380f1402e91c1ed5b91
PIara-Adviters <capacitaciones4@adviters.com>
2023-04-05 16:13:12 -0300

    Agrega componentes material

--- src/Pages/CargarLicenciaPage/CargarLicenciaPage.jsx
+++ src/Pages/CargarLicenciaPage/CargarLicenciaPage.jsx
@@ -1,82 +1,118 @@
 import React from "react";
-import { Avatar, Badge, Select, MenuItem, InputLabel, ButtonGroup, Button, DatePicker } from "@mui/material";
+import {
+  Avatar,
+  Badge,
+  Select,
+  MenuItem,
+  InputLabel,
+  ButtonGroup,
+  Button,
+  DatePicker,
+  Typography,
+  Grid,
+  TextareaAutosize,
+} from "@mui/material";
 import { DateRangePicker } from "@mui/lab";
 import scaloni from "./scaloni.jpg";
 import "./CargarLicenciaStyle.css";
 
 const CargarLicenciaPage = () => {
   return (
-    <>
-      <main id="contenedor">
-        <DateRangePicker/>
-        <section id="titulo">
-          <div>
-            <Avatar alt="scaloni" src={scaloni} />
-            <p>Ricky</p>
-          </div>
-          <div>
-            <p>
-              Balance actual: <b>24 días</b>
-            </p>
-          </div>
-          <div>
-            <p>ESTADO</p>
-            <Badge badgeContent={"Aún no enviado"} color="success"></Badge>
-          </div>
-        </section>
-        <section id="carga-licencia">
-          <div>
-            <span>
-              <InputLabel id="demo-select-small">TIPO DE LICENCIA</InputLabel>
-              <Select labelId="demo-select-small" value="None">
-                <MenuItem value="None">
-                  <em>None</em>
-                </MenuItem>
-                <MenuItem value={10}>Ten</MenuItem>
-                <MenuItem value={20}>Twenty</MenuItem>
-                <MenuItem value={30}>Thirty</MenuItem>
-              </Select>
-            </span>
-            <span>
-              <h3>Archivo adjuntos</h3>
-              <p>No hay archivos adjuntos todavia</p>
-              <Button variant="contained">SUBIR ARCHIVO</Button>
-            </span>
-          </div>
-          <div id="fecha-licencia">
-            <span className="calendario-widget">
+    <Grid container>
+      <Grid sm={12} md={10}>
+        <main id="contenedor">
+          <section id="titulo">
+            <div>
+              <Avatar alt="scaloni" src={scaloni} />
+              <Typography variant="overline">Ricky</Typography>
+            </div>
+            <div>
+              <Typography variant="h6">
+                Balance actual: <b>24 días</b>
+              </Typography>
+            </div>
+            <div>
+              <Typography variant="button" display={"block"}>
+                ESTADO
+              </Typography>
+              <Badge badgeContent={"Aún no enviado"} color="success"></Badge>
+            </div>
+          </section>
+          <section id="carga-licencia">
+            <div>
+              <span>
+                <InputLabel id="demo-select-small">TIPO DE LICENCIA</InputLabel>
+                <Select labelId="demo-select-small" value="None">
+                  <MenuItem value="None">
+                    <em>Licencias</em>
+                  </MenuItem>
+                  {/* cambiar esto */}
+                  <MenuItem value={10}>Ten</MenuItem>
+                  <MenuItem value={20}>Twenty</MenuItem>
+                  <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+              </span>
+              <span>
+                <Typography variant="h6">Archivo adjuntos</Typography>
+                <Typography variant="caption" display="block">
+                  No hay archivos adjuntos todavia
+                </Typography>
+                <Button variant="contained">SUBIR ARCHIVO</Button>
+              </span>
+            </div>
+            <div id="fecha-licencia">
+              <span className="calendario-widget">
                 <span>{"Septiembre"}</span>
-                <span>{21}{" Lunes"}</span>
-            </span>
-            <span className="calendario-widget">
+                <span>
+                  {21}
+                  {" Lunes"}
+                </span>
+              </span>
+              <span className="calendario-widget">
                 <span>{"Septiembre"}</span>
-                <span>{21}{" Lunes"}</span>
-            </span>
-            <ButtonGroup id="grupo-botones-licencia">
-              <Button>{8} dias laborales.</Button>
-              <Button>{24} dias disponibles</Button>
-            </ButtonGroup>
-          </div>
-          <div id="descripcion-razon-licencia">
-            <h4>DESCRIPCION</h4>
-           <textarea placeholder="Viaje al centro de la tierra" rows="4" cols="50"></textarea> 
-          </div>
-        </section>
-        <section id="responsable-licencia">
-          <p>Aprobación a cargo de:</p>
-          <div id="contenedor-usuario">
-            <Avatar alt="avatar-usuario" src={scaloni} />
-            <p>LIONEL SCALONI</p>
-          </div>
-          <Button variant="contained" id="boton-licencia">
-            SOLICITAR APROBACION
-          </Button>
-        </section>
-      </main>
-      <aside>
+                <span>
+                  {21}
+                  {" Lunes"}
+                </span>
+              </span>
+              <ButtonGroup
+                id="grupo-botones-licencia"
+                size="small"
+                color="primary"
+              >
+                <Button>{8} dias laborales.</Button>
+                <Button>{24} dias disponibles</Button>
+              </ButtonGroup>
+            </div>
+            <div>
+              <Typography variant="h6">DESCRIPCION</Typography>
+              <textarea
+                placeholder="Viaje al centro de la tierra"
+                rows="4"
+                cols="50"
+              ></textarea>
+            </div>
+          </section>
+          <section id="responsable-licencia">
+            <Typography variant="h6" align="left">
+              Aprobación a cargo de:
+            </Typography>
+            <div id="contenedor-usuario">
+              <Avatar alt="avatar-usuario" src={scaloni} />
+              <Typography variant="overline">LIONEL SCALONI</Typography>
+            </div>
+            <Button variant="contained" id="boton-licencia">
+              SOLICITAR APROBACION
+            </Button>
+          </section>
+        </main>
+      </Grid>
 
-      </aside>
-    </>
+      <Grid sm={12} md={2}>
+      <aside></aside>
+      </Grid>
+      
+    </Grid>
   );
 };
 
