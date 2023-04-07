e3a4aae53c08dd978635ac06d0b6c0fcf2e00493
Iara Pou <129104941+PIara-Adviters@users.noreply.github.com>
2023-04-07 13:55:49 -0300

    Cambia componente container Lista
    Con el fin de que sea más genérico (y reutilizable para los dos tipos de listas que existen, de pedidos de licencias y de licencias procesadas).

--- src/components/lista/Lista.jsx
+++ src/components/lista/Lista.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import "./lista.css";
-import ListItem from "../ListItem/ListItem";
 
-const Lista = ({ titulo, data, displayIconos }) => {
+const Lista = ({ titulo, children}) => {
   return (
     <div className="l-flexListItem">
       <div className="l-flexItem">
         <h1 className="l-titulo">{titulo}</h1>
       </div>
-      {data.map((el, index) => (
-        <ListItem data={el} key={index} displayIconos={displayIconos} />
-      ))}
+      {children}
     </div>
   );
 };
