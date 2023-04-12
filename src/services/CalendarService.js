7befdd0aa6746b1c52768d5ce01703eb814d9468
guerrijoaquin <joaquinguerrini1@gmail.com>
2023-04-12 12:40:47 -0300

    changes

--- src/services/CalendarService.js
+++ src/services/CalendarService.js
@@ -5,3 +5,6 @@ export const getHolidays = () => httpRequest.get('/calendar/holidays')
 
 //SAVE NEW HOLIDAY
 export const newHoliday = (name, date) => httpRequest.post('/calendar/holidays/new', {name, date})
+
+//DELETE A HOLIDAY
+export const deleteHoliday = (holidayId) => httpRequest.delete(`/calendar/holidays/${holidayId}`)
