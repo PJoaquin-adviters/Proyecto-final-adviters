abd4c83a1eac569ac9e6f78fe8a26a8055165035
Iara Pou <129104941+PIara-Adviters@users.noreply.github.com>
2023-04-17 22:38:08 -0300

    Crea componente UserTypeProvider y UserContext

---
+++ src/context/UserTypeContext.jsx
@@ -0,0 +1,18 @@
+import { createContext, useState } from "react";
+
+const UserTypeContext = createContext();
+
+const UserTypeProvider = ({ children }) => {
+    //0 = supervisor
+    //1 = usuario
+  const [idRol, setIdRol] = useState(1);
+
+  return (
+    <UserTypeContext.Provider value={{ idRol, setIdRol }}>
+      {children}
+    </UserTypeContext.Provider>
+  );
+};
+
+export {UserTypeProvider};
+export default UserTypeContext;
