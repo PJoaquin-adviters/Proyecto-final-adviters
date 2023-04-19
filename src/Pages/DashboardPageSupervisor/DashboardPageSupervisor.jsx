5ac4b721012f9c7c074c1475219c873fe2677d9e
Capacitaciones 9 <capacitaciones9@adviters.com>
2023-04-19 15:12:22 -0300

    hola

--- src/Pages/DashboardPageSupervisor/DashboardPageSupervisor.jsx
+++ src/Pages/DashboardPageSupervisor/DashboardPageSupervisor.jsx
@@ -1,4 +1,4 @@
-import { ListItem } from "@mui/material";
+import { ListItem, TextField } from "@mui/material";
 import ListItemCargaLicencia from "../../components/ListItemCargaLicencia/ListItemCargaLicencia";
 import Lista from "../../components/lista/Lista";
 import "./DashboardPageSupervisor.css";
@@ -48,41 +48,19 @@ const DashboardPage = () => {
       endDate: "2023-02-18",
       type: "Vacaciones",
     },
-    {
-      username: "jordi",
-      lastname: "joe",
-      img: "fgdfg",
-      startDate: "2023-02-18",
-      endDate: "2023-02-18",
-      type: "Vacaciones",
-    },
-    {
-      username: "flex",
-      lastname: "joe",
-      img: null,
-      startDate: "2023-02-18",
-      endDate: "2023-02-18",
-      type: "Vacaciones",
-    },
-    {
-      username: "jordi",
-      lastname: "joe",
-      img: "fgdfg",
-      startDate: "2023-02-18",
-      endDate: "2023-02-18",
-      type: "Vacaciones",
-    },
   ];
   return (
-
     <section className="dashboardSup-container-gral">
-      
       <div className="dashboardSup-mockup-container">
         <MockupWeather></MockupWeather>
         <MockupCalendario></MockupCalendario>
       </div>
 
       <div className="lista-dashboard-container">
+        <div className="hola">
+          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        </div>
+
         <div className="lista-dashboard">
           <Lista titulo="Solicitudes Pendientes">
             {solicitudesPendientes.map((el, index) => (
@@ -90,7 +68,7 @@ const DashboardPage = () => {
             ))}
           </Lista>
         </div>
-        <div className="lista-dashboard">
+        <div className="lista-dashboard-licencias-aprobadas">
           <Lista titulo="Proximas licencias aprobadas">
             {solicitudesPendientes.map((el, index) => (
               <ListItemSolicitudes data={el} displayIconos={false} />
