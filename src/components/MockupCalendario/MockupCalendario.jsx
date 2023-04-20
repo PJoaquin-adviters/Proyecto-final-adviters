52a95bf52fed1fc6ad6c8ab6425200dc54308dab
PIara-Adviters <capacitaciones4@adviters.com>
2023-04-20 13:31:16 -0300

    Crea calendario
    Marca fechas de la api de feriados

--- src/components/MockupCalendario/MockupCalendario.jsx
+++ src/components/MockupCalendario/MockupCalendario.jsx
@@ -1,13 +1,57 @@
-import React from "react";
-import HolidayCalendar from "../HolidayCalendar/HolidayCalendar";
-import { Typography } from "@mui/material";
-import "./MockupCalendarioStyles.css";
+import React, { useEffect, useState } from "react";
+import FullCalendar from "@fullcalendar/react"; // must go before plugins
+import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
+import CalendarService from "../../services/CalendarService";
 
 const MockupCalendario = () => {
+  const [holidays, setHolidays] = useState([]);
+
+  const getHolidays = async () => {
+    try {
+      const feriadosRecibidos = await CalendarService.getHolidays();
+
+      let mappedFeriados = [];
+
+      feriadosRecibidos.data.map((feriado) => {
+        mappedFeriados.push({
+          start: feriado.date,
+          end: feriado.date,
+          display: "background",
+          color: "blue",
+        });
+      });
+
+      console.log(mappedFeriados);
+      setHolidays(mappedFeriados);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getHolidays();
+  }, []);
+
   return (
-    <div className="mock-calendar-container">
-      <HolidayCalendar></HolidayCalendar>
-      
+    <div
+      id="calendar"
+      style={{
+        width: "400px",
+        overflow: "hidden",
+        height: "auto",
+      }}
+    >
+      <FullCalendar
+        plugins={[dayGridPlugin]}
+        headerToolbar={{
+          left: "prev",
+          center: "title",
+          right: "next",
+        }}
+        initialView="dayGridMonth"
+        //que los días sean una letra
+        events={holidays}
+      />
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { UserDataProvider } from "./context/UserDataContext";
+import MockupCalendario from "./components/MockupCalendario/MockupCalendario";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <UserDataProvider>
-      <App />
+      <MockupCalendario></MockupCalendario>
+      {/* <App /> */}
     </UserDataProvider>
   </React.StrictMode>
 );
