1a1e36ecf6001c855153f6d3ca2c6541ba6c95da
Iara Pou <129104941+PIara-Adviters@users.noreply.github.com>
2023-04-20 22:43:10 -0300

    Reduce altura de días en el calendario

--- src/index.js
+++ src/index.js
@@ -9,8 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <UserDataProvider>
-      <MockupCalendario></MockupCalendario>
-      {/* <App /> */}
+      <App />
     </UserDataProvider>
   </React.StrictMode>
 );
